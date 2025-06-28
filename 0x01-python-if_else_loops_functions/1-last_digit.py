cdf6b818e7ea5402756a78bd28540c51a09c8152
Nickson Rono <nickson@roycetechnologies.co.ke>
2025-06-28 16:16:24 +0300

    turned last_digit to string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+last_digit = str(number)[-1] 
 print(f"Last digit of {number} is {last_digit}", end="")
 
 if (int(last_digit) > 5):
     print(" and is greater than 5")
-elif(int(last_digit) == 0):
+elif (int(last_digit) == 0):
     print(" and is 0")
 elif (int(last_digit) < 6 and int(last_digit) != 0):
     print(" and is less than 6 and not 0")
