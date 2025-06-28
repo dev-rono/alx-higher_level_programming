5a63584663c0a916b18bbe30b8e602e272d91a24
Nickson Rono <nickson@roycetechnologies.co.ke>
2025-06-28 20:28:19 +0300

    fixed syntax error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 
 for num in range(100):
     if num < 10:
-        print("0{}".format(num), end=", " if num != 99)
+        print("0{}".format(num), end=", " if num != 99 else "")
     else:
-        print("{}".format(num), end=", " if num != 99)
+        print("{}".format(num), end=", " if num != 99 else "")
 
