274c82e478b06d170258ebcbb548f2898987df20
Nickson Rono <nickson@roycetechnologies.co.ke>
2025-06-28 20:15:59 +0300

    formatted code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for num in range(100):
-    if (len(str(num)) == 1):
-        print("0{}".format(num), end = ", " if num != 99 else " ")
+    if num < 10:
+        print("0{}".format(num), end=", " if num != 99 else " ")
     else:
         print("{}".format(num), end=", " if num != 99 else " ")
