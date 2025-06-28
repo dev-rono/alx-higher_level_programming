56c8d34ec9833f253c92ac5ee2c877dc7ad74838
Nickson Rono <nickson@roycetechnologies.co.ke>
2025-06-28 20:10:11 +0300

    removed trailing commas

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for num in range(100):
     if (len(str(num)) == 1):
-        print("0{}".format(num), end = ", " if num != 99 else "")
+        print("0{}".format(num), end = ", " if num != 99 else " ")
     else:
-        print("{}".format(num), end=", " if num != 99 else "")
+        print("{}".format(num), end=", " if num != 99 else " ")
