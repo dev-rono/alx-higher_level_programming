d934d4d7bcbdcca6dc07dedcebb8685460cb6617
Nickson Rono <nickson@roycetechnologies.co.ke>
2025-07-01 23:43:11 +0300

    add print statements to debug

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,11 @@ def uppercase(str):
     new_str = ""
     for char in str:
         char = ord(char)
+        print("char 0: ", char)
         if (char > 96):
+            print("char 1: ", char)
             char -= 32
+            print("char 2: ", char)
         new_str += chr(char)
     print("{}".format(new_str))
-print()
+uppercase("alx")
