cd8ecc71da62633ee13c6b0d96127d1149c46fb5
Nickson Rono <nickson@roycetechnologies.co.ke>
2025-07-04 22:48:07 +0300

    print all uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    new_str = ""
     for char in str:
         char = ord(char)
         if 97 <= char <= 122:
             char -= 32
-        new_str += chr(char)
-    print("{}".format(new_str))
-print()
+        char = chr(char)
+        print("{}".format(char), end="")
+    print()
